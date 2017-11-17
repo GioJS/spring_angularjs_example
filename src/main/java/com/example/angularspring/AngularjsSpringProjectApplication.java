7a27589d145e541891062e18edbd7b428072086c
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-11-17 17:58:23 +0100

    comment

--- src/main/java/com/example/angularspring/AngularjsSpringProjectApplication.java
+++ src/main/java/com/example/angularspring/AngularjsSpringProjectApplication.java
@@ -3,7 +3,7 @@ package com.example.angularspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//spring-boot requires spring-4 but differs a little bit from 3.x in this basic application
 
 @SpringBootApplication
 public class AngularjsSpringProjectApplication {
