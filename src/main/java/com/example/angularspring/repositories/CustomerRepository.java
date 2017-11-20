fa2cd371f86b1378ef138f5d6cc8b23727502c5f
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-11-20 11:02:33 +0100

    third commit

--- src/main/java/com/example/angularspring/repositories/CustomerRepository.java
+++ src/main/java/com/example/angularspring/repositories/CustomerRepository.java
@@ -4,7 +4,6 @@ import com.example.angularspring.repositories.utils.GroupByEntry;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.angularspring.entities.Customer;
--- src/main/java/com/example/angularspring/repositories/ProductRepository.java
+++ src/main/java/com/example/angularspring/repositories/ProductRepository.java
@@ -5,7 +5,6 @@ import com.example.angularspring.repositories.utils.GroupByEntry;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
