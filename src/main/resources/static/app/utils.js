07149208bd19138a0a4005266bef00d21aa93a32
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-11-21 12:25:41 +0100

    refactoring++++

--- src/main/resources/static/app/utils.js
+++ src/main/resources/static/app/utils.js
@@ -1,9 +1,6 @@
 function watcher($scope, $elem, $attrs) {
-    //console.log($attrs)
     $scope.$watch($attrs.ngShow, function(newVal, oldVal) {
-        //console.log(newVal, oldVal)
         if(newVal !== oldVal) {
-            //console.log('namo');
             $scope.editable = false;
         }
 
