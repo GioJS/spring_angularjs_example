35bf7b30d9505d6485b4b68fed26e0fe98c7fc62
giordano.cristini@gmail.com <giordano.cristini@gmail.com>
2017-11-21 09:40:35 +0100

    refactoring++

--- src/main/java/com/example/angularspring/controllers/StatsController.java
+++ src/main/java/com/example/angularspring/controllers/StatsController.java
@@ -7,7 +7,6 @@ import com.example.angularspring.repositories.utils.GroupByEntry;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
